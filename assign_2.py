4d770573559925bb3bd40367807105035da8446e
Prathmesh3742 <138551124+Prathmesh3742@users.noreply.github.com>
2023-07-07 10:32:57 +0530

    Add files via upload

---
+++ assign_2.py
@@ -0,0 +1,88 @@
+# Arithmetic operator
+print("Arithmetic operator")
+a= 23
+b= 74
+print("addition :",a+b)
+print("Subraction :",a-b)
+print("Multiplication :",a*b)
+print("Division :",a/b)
+print("Exponentpy :",a**b)
+print("Floor Division :",a//b)
+
+print()
+
+# Assignment operator
+print("Assignment operator")
+c=45
+c+=3
+print("45+=3",c)
+c=45
+c-=3
+print("45-=3",c)
+c=45
+c*=3
+print("45*=3",c)
+c=45
+c/=3
+print("45/=3",c)
+c=45
+c%=3
+print("45%=3",c)
+c=45
+c//=3
+print("45//=3",c)
+c=45
+c**=3
+print("45**=3",c)
+
+print()
+
+# Comparison operator
+print("Comparison operator")
+a = 23
+b= 74
+print("23>74 = ",a>b )
+print("23<74 = ",a<b )
+print("23>=74 = ",a>=b )
+print("23<=74 = ",a<=b )
+print("23==74 = ",a==b )
+print("23!=74 = ",a!=b )
+
+print()
+
+# Logical operator
+print("Logical operator")
+l =True
+m= False
+print(l,m)
+print("True and False =",l and m)
+print("True or False =",l or m)
+print("True l =",not l)
+
+print()
+
+# Bitwise operator
+print("Bitwise operator")
+print("23 | 74 =",a | b)
+print("23 & 74 =",a & b)
+print("23 ^ 74 =",a ^ b)
+print("23 >> 2 =",a >> 2)
+print("23 << 2 =",a << 2)
+
+print()
+
+# Identity operator
+print("Identity operator")
+x =10
+print("x:10")
+print("x is 10 =",x is 10)
+print("x is not 10 =",x is not 10)
+
+print()
+
+# Membership operator
+print("Membership operator")
+d = "Temp string"
+print("d =",d)
+print("e in d",("e" in d))
+print("e not in d",("e" not in d))
