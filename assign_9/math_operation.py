fead20c3653d5ac26d46c1e2f12ed9d1b3435c7b
Prathmesh3742 <138551124+Prathmesh3742@users.noreply.github.com>
2023-07-14 14:55:53 +0530

    Update math_operation.py

--- assign_9/math_operation.py
+++ assign_9/math_operation.py
@@ -17,3 +17,16 @@ class math_operations :
         print(f"Cosine of {self.b} :",m.cos(self.b))
         print(f"Tan of {self.a} :",m.tan(self.a))
         print(f"Tan of {self.b} :",m.tan(self.b))
+    
+    def basic(self):
+        print(f"Additon of {self.a} and {self.b} is:",self.a + self.b)
+        print(f"Subtraction of {self.a} and {self.b} is:",self.a - self.b)
+        print(f"Multiplication of {self.a} and {self.b} is:",self.a * self.b)
+        print(f"Division of {self.a} and {self.b} is:",self.a / self.b)
+
+    def rest(self):
+        print(f"Factorial of {self.a} is:",m.factorial(self.a))
+        print(f"Factorial of {self.b} is:",m.factorial(self.b))
+        print("Floor of 8.4 :",m.floor(8.4))
+        print("Ceil of 21.8 :",m.ceil(21.8))
+        print("Sum of list [4,5,6,7] is :",m.fsum([4,5,6,7]))
